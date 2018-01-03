b7f5a987a5f22223bc2b0d083e626302de51d0ba
davy <davy.wybiral@gmail.com>
2018-01-02 19:24:39 -0600

    Added iframe_002

---
+++ iframe_001/main.py
@@ -0,0 +1,20 @@
+from os import urandom
+from base64 import b64encode
+from tornado import ioloop
+from tornado import web
+
+class IndexHandler(web.RequestHandler):
+    @web.asynchronous
+    def get(self):
+        while True:
+            for i in range(100):
+                self.write('<iframe src="data:application/octet-stream;base64,')
+                self.write(b64encode(urandom(16)))
+                self.write('"></iframe>')
+            self.flush()
+
+app = web.Application([(r'/', IndexHandler)])
+
+if __name__ == "__main__":
+    app.listen(8080)
+    ioloop.IOLoop.current().start()
